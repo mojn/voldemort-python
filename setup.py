4dc83498ed522d0a2a1ecba740c6f18d979cf1cc
Magnus Rønager <mar@mojn.com>
2014-12-11 10:09:04 +0100

    removed outcommented sections

--- setup.py
+++ setup.py
@@ -10,26 +10,3 @@ setup(
     author_email = 'sh@mojn.com',
     url = 'https://github.com/mojn/voldemort-python'
 )
-
-# if __name__ == "__main__":
-    
-    # jar_file_name = 'voldemort-python.jar'
-    # if 'install' in sys.argv:
-    #     try:
-    #         check_call('gradle shadowJar', shell=True)
-    #     except CalledProcessError:
-    #         raise IOError("Could not build java client. Is gradle installed?")
-    #     base = os.path.dirname(__file__)
-    #     lib_path = os.path.join(base, 'build', 'libs')
-    #     lib_file = None
-    #     for f in os.listdir(lib_path):
-    #         if f.startswith('voldemort-python') and f.endswith('-all.jar'):
-    #             lib_file = os.path.join(lib_path, f)
-    #             break;
-    #     if not lib_file:
-    #         raise IOError('Could not find voldemort-python*-all.jar file')
-    #     os.rename(lib_file, os.path.join(base, 'jvoldemort', jar_file_name))
-    
-    # packages = ['jvoldemort']
-    # packages = packages + list(chain.from_iterable( tuple( p + '.' + n for n in find_packages(p) ) for p in packages ))
-    
